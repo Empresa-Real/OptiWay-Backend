bde888a1bd8c5886dcbaf937cf05b97927893b10
ema28pro <ema2805pro@gmail.com>
2026-09-17 23:58:45 -0500

    initial commit

---
+++ src/test/java/com/example/optiway/OptiwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.optiway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptiwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
