42b1a0748bc65b764ba628b43cc5835f02f8004b
ema28pro <ema2805pro@gmail.com>
2026-09-18 11:56:24 -0500

    ajustes a la tabla de usuarios

--- src/main/java/com/example/optiway/infraestructure/adapter/out/persistence/UsuarioJpaEntity.java
+++ src/main/java/com/example/optiway/infraestructure/adapter/out/persistence/UsuarioJpaEntity.java
@@ -2,10 +2,15 @@ package com.example.optiway.infraestructure.adapter.out.persistence;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+
+import com.example.optiway.domain.model.Rol;
+import java.util.UUID;
 
 @Entity
 @Table(name = "usuarios")
@@ -16,16 +21,21 @@ public class UsuarioJpaEntity {
     private String nombre;
     @Column(nullable = false, unique = true)
     private String email;
-    private String rol;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Rol rol;
+    @Column(name = "creado_por")
+    private UUID creadoPor;
 
     public UsuarioJpaEntity() {
     }
 
-    public UsuarioJpaEntity (Long id, String nombre, String email, String rol) {
+    public UsuarioJpaEntity (Long id, String nombre, String email, Rol rol, UUID creadoPor) {
         this.id = id;
         this.nombre = nombre;
         this.email =  email;
         this.rol = rol;
+        this.creadoPor = creadoPor;
     }
 
     public Long getId() {
@@ -52,12 +62,20 @@ public class UsuarioJpaEntity {
         this.email = email;
     }
 
-    public String getRol() {
+    public Rol getRol() {
         return rol;
     }
 
-    public void setRol(String rol) {
+    public void setRol(Rol rol) {
         this.rol = rol;
+    }
+
+    public UUID getCreadoPor() {
+        return creadoPor;
+    }
+
+    public void setCreadoPor(UUID creadoPor) {
+        this.creadoPor = creadoPor;
     }
 }
 
