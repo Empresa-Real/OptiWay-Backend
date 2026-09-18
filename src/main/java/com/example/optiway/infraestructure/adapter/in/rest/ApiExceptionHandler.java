abfdc97328b1cac850948fa4e1f89de9f0dc5d43
ema28pro <ema2805pro@gmail.com>
2026-09-18 13:52:18 -0500

    Seguridad y Exceptions

--- src/main/java/com/example/optiway/infraestructure/adapter/in/rest/ApiExceptionHandler.java
+++ src/main/java/com/example/optiway/infraestructure/adapter/in/rest/ApiExceptionHandler.java
@@ -7,10 +7,24 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.example.optiway.application.service.AccesoDenegadoException;
+import com.example.optiway.application.service.InvitacionAuthException;
 import com.example.optiway.application.service.UsuarioDuplicadoException;
 
 @RestControllerAdvice
 public class ApiExceptionHandler {
+
+    @ExceptionHandler(InvitacionAuthException.class)
+    @ResponseStatus(HttpStatus.BAD_GATEWAY)
+    public Map<String, String> invitacionFallida(InvitacionAuthException exception) {
+        return Map.of("error", exception.getMessage());
+    }
+
+    @ExceptionHandler(AccesoDenegadoException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public Map<String, String> accesoDenegado(AccesoDenegadoException exception) {
+        return Map.of("error", exception.getMessage());
+    }
 
     @ExceptionHandler(UsuarioDuplicadoException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
